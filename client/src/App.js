454efdc02cd4ce457cf2acd3a4d9957ad5e83f24
acotterson <acotterson@users.noreply.github.com>
2023-08-30 00:45:54 -0600

    made a basic homepage and updated package.json and such

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Nav from './components/Nav/index';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+            />
+             <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
